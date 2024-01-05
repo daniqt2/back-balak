a84ce67b3e1b3cef95b84ae71c76d740f5521ea2
Dani Quesada <daniela.dudyfit@gmail.com>
2024-01-05 23:00:04 +0100

    remove db

--- app/db/mongodb.py
+++ app/db/mongodb.py
@@ -10,8 +10,8 @@ async def get_database() -> AsyncIOMotorClient:
     return db.client
 
 async def connect_to_mongo():
-    db.client = AsyncIOMotorClient(str('mongodb+srv://bajak-cluster:5rRe2xx5nQKOIbU4@cluster0.fbenkdc.mongodb.net/?retryWrites=true&w=majority')) ## get frmo compose
-
+    db.client = AsyncIOMotorClient(str()) ## get frmo compose
 
 async def close_mongo_connection():
     db.client.close()
+    
